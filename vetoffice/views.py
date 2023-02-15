8a2cdd2adac51f79641ef97edac0aa47b05e7138
ccp84 <ccp84@icloud.com>
2023-02-15 18:41:33 +0000

    py>add view for home

--- vetoffice/views.py
+++ vetoffice/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def home(request):
+    context = {"name": "Spot"}
+    return render(request, "home.html", context)
