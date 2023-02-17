1b619199152376564dec21324c261723730a8f6a
ccp84 <ccp84@icloud.com>
2023-02-17 15:50:15 +0000

    py>add pet list to context

--- vetoffice/views.py
+++ vetoffice/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+pets = [
+  { "petname": "Fido", "animal_type": "dog"},
+  { "petname": "Clementine", "animal_type": "cat"},
+  { "petname": "Cleo", "animal_type": "cat"},
+  { "petname": "Oreo", "animal_type": "dog"},
+]
 
 def home(request):
-    context = {"name": "Spot"}
+    context = {
+        "name": "Dr Vet"
+        "pets": pets}
     return render(request, "home.html", context)
