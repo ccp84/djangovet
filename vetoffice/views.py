3194e3e913698939d4b8b56c9a83835d0df15da2
ccp84 <ccp84@icloud.com>
2023-02-18 18:15:28 +0000

    py>add contect vars

--- vetoffice/views.py
+++ vetoffice/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 from django.template import loader
 
 pets = [
-  { "petname": "Fido", "animal_type": "dog"},
-  { "petname": "Clementine", "animal_type": "cat"},
-  { "petname": "Cleo", "animal_type": "cat"},
-  { "petname": "Oreo", "animal_type": "dog"},
+    {"petname": "Fido", "animal_type": "dog"},
+    {"petname": "Clementine", "animal_type": "cat"},
+    {"petname": "Cleo", "animal_type": "cat"},
+    {"petname": "Oreo", "animal_type": "dog"},
 ]
+
 
 def home(request):
     context = {
-        "name": "Dr Vet"
-        "pets": pets}
+        "name": "Dr Vet",
+        "pets": pets
+    }
     return render(request, "home.html", context)
