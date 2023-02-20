43d31f9875d6844334c29682bacbfd05606367f9
ccp84 <ccp84@icloud.com>
2023-02-20 11:34:16 +0000

    py>create owner and patient models

--- vetoffice/models.py
+++ vetoffice/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+
+
+class Patient(models.Model):
+    breed = models.CharField(max_length=200)
+    pet_name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
