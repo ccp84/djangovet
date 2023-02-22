e206fc9af235eddcb2a42b91fb159ad2ddd51feb
ccp84 <ccp84@icloud.com>
2023-02-22 12:19:13 +0000

    html>update templates

--- vetoffice/urls.py
+++ vetoffice/urls.py
@@ -2,5 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-  path("", views.home)
+  path("", views.home, name="home"),
+  path("owner/list", views.OwnerList.as_view(), name="ownerlist"),
+  path("patient/list", views.PatientList.as_view(), name="patientlist"),
+  path("owner/create", views.OwnerCreate.as_view(), name="ownercreate"),
+  path("patient/create", views.PatientCreate.as_view(), name="patientcreate"),
+  path("owner/update/<pk>", views.OwnerUpdate.as_view(), name="ownerupdate"),
+  path("patient/update/<pk>", views.PatientUpdate.as_view(), name="patientupdate"),
+  path("owner/delete/<pk>", views.OwnerDelete.as_view(), name="ownerdelete"),
+  path("patient/delete/<pk>", views.PatientDelete.as_view(), name="patientdelete"),
 ]
--- vetoffice/views.py
+++ vetoffice/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Owner, Patient
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 pets = [
     {"petname": "Fido", "animal_type": "dog"},
@@ -16,3 +19,47 @@ def home(request):
         "pets": pets
     }
     return render(request, "home.html", context)
+
+
+class OwnerList(ListView):
+    model = Owner
+    template_name = "owner_list.html"
+
+
+class PatientList(ListView):
+    model = Patient
+    template_name = "patient_list.html"
+
+
+class OwnerCreate(CreateView):
+    model = Owner
+    template_name = "owner_create_form.html"
+    fields = ["first_name", "last_name", "phone"]
+
+
+class PatientCreate(CreateView):
+    model = Patient
+    template_name = "patient_create_form.html"
+    fields = ["animal_type", "breed", "pet_name", "age", "owner"]
+
+
+class OwnerUpdate(UpdateView):
+    model = Owner
+    template_name = "owner_update_form.html"
+    fields = ["first_name", "last_name", "phone"]
+
+
+class PatientUpdate(UpdateView):
+    model = Patient
+    template_name = "patient_update_form.html"
+    fields = ["animal_type", "breed", "pet_name", "age", "owner"]
+
+
+class OwnerDelete(DeleteView):
+    model = Owner
+    template_name = "owner_delete_form.html"
+
+
+class PatientDelete(DeleteView):
+    model = Patient
+    template_name = "patient_delete_form.html"
